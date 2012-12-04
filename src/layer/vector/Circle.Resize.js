2b14ba63a3e065d25b627f9d124ca7b48a7eec02
dreamfall <amarant.st@gmail.com>
2012-12-04 18:56:11 +0300

    fix draggable point position calculations

--- src/layer/vector/Circle.Resize.js
+++ src/layer/vector/Circle.Resize.js
@@ -9,7 +9,7 @@ L.Handler.CircleResize = L.Handler.extend({
             className: 'leaflet-div-icon leaflet-editing-icon'
         })
     },
-    
+
     initialize: function (circle, options) {
         this._circle = circle;
         L.Util.setOptions(this, options);
@@ -17,14 +17,14 @@ L.Handler.CircleResize = L.Handler.extend({
 
     addHooks: function () {
         if (this._circle._map) {
-            
+
             // display handler
             this._markerGroup = new L.LayerGroup();
             this._circle._map.addLayer(this._markerGroup);
             this._initHandler();
         }
     },
-    
+
     removeHooks: function () {
         if (this._circle._map) {
             this._circle.off('drag', this._updateHandler, this);
@@ -33,65 +33,85 @@ L.Handler.CircleResize = L.Handler.extend({
             delete this._origins;
         }
     },
-    
+
     _initHandler: function () {
         // define handler (icon and position)
         var icon = this.options.icon,
             bounds = this._circle.getBounds(),
             handlerll = new L.LatLng(this._circle.getLatLng().lat, bounds.getNorthEast().lng);
-            
+
         this._dragHandler = new L.Marker(handlerll, {
                 icon: icon,
                 draggable: true
             });
-        
+
         // define handler events
         this._dragHandler
             .on('dragstart', this._onDragStart, this)
             .on('drag', this._onDrag, this)
             .on('dragend', this._onDragEnd, this);
-        
+
         this._circle.on('drag', this._updateHandler, this);
+        this._circle._map.on('zoomend', this._setOrigins, this);
         this._markerGroup.addLayer(this._dragHandler);
         this._setOrigins();
     },
-    
+
     _updateHandler: function (e) {
         var originc = this._origins.circle,
             originh = this._origins.handler,
             newc = this._circle._map.project(this._circle.getLatLng()),
+
             delta = {
-                x: newc.x - originc.x,
-                y: newc.y - originc.y
-            };
-        
-        var hpoint = new L.Point(originh.x + delta.x, originh.y + delta.y);
+                x: originh.x - originc.x,
+                y: originh.y - originc.y
+            },
+            distanceInPxRatio = this._origins.cos / Math.cos(this._circle.getLatLng().lat * L.LatLng.DEG_TO_RAD);
+
+        delta.x = delta.x * distanceInPxRatio;
+        delta.y = delta.y * distanceInPxRatio;
+
+        var newXY = {
+            x: newc.x + delta.x,
+            y: newc.y + delta.y
+        };
+
+        var hpoint = new L.Point(newXY.x, newXY.y);
         this._dragHandler.setLatLng(this._circle._map.unproject(hpoint));
     },
-    
+
     _onDragStart: function (e) {
     },
-    
+
     _onDrag: function (e) {
         var circleCenter = this._circle.getLatLng(),
-            handlerPos = e.target.getLatLng();
-        this._circle.setRadius(circleCenter.distanceTo(handlerPos));
+            centerPos = this._circle._map.project(circleCenter),
+            handlerXY = this._circle._map.project(e.target.getLatLng()),
+            handlerPos = {
+                x: handlerXY.x,
+                y: handlerXY.y
+            },
+            radiusInPix = Math.sqrt(Math.pow(centerPos.x - handlerPos.x, 2) + Math.pow(centerPos.y - handlerPos.y, 2)),
+            point = new L.Point(centerPos.x + radiusInPix, centerPos.y);
+
+        this._circle.setRadius(circleCenter.distanceTo(this._circle._map.unproject(point)));
         this._circle.fire('resize');
     },
-    
+
     _onDragEnd: function () {
         this._setOrigins();
     },
-    
+
     _setOrigins: function () {
         // memorize circle and handler origins
         if (typeof(this._circle._map) !== 'undefined' && this._circle._map instanceof L.Map) {
             var map = this._circle._map;
             this._origins = {
+                cos:  Math.cos(this._circle.getLatLng().lat * L.LatLng.DEG_TO_RAD),
                 circle: map.project(this._circle.getLatLng()),
                 handler: map.project(this._dragHandler.getLatLng())
             };
         }
     }
-    
+
 });
